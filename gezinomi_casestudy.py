f86c51c814ce81204f15b4a549527d4e310d1a36
Nisa Nur <73359949+nisanurtas@users.noreply.github.com>
2025-06-04 13:33:38 +0300

    Update gezinomi_casestudy.py

--- gezinomi_casestudy.py
+++ gezinomi_casestudy.py
@@ -1,38 +1,38 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_excel(r"C:\Users\HP\PycharmProjects\PythonProject\gezinomi_tanıtım\miuul_gezinomi.xlsx")
+df = pd.read_excel("miuul_gezinomi.xlsx")
 df.head()
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width',500)
 
-#Soru2:Kaç unique şehirvardır? Frekanslarınedir
+# Question2:How many unique cities are there? What are their frequencies?
 df["SaleCityName"].unique()
 df["SaleCityName"].value_counts()
 
-# Soru3:Kaç unique Concept vardır?
+# Question 3: How many unique Concepts are there?
 df["ConceptName"].nunique()
 
-#Soru4: Hangi Concept’den kaçar tane satış gerçekleşmiş?
+# Question 4: How many sales were made from which Concept?
 df["ConceptName"].value_counts()
 
-# Soru5:Şehirlere göre satışlardan toplam ne kadar kazanılmış?
+# Question 5: How much total was earned from sales by city?
 df.groupby("SaleCityName").agg({"Price" : "sum"})
 
-#Soru6:Concept türlerine göre göre ne kadar kazanılmış?
+# Question 6: How much was earned according to concept types?
 df.groupby("ConceptName").agg({"Price" : "sum"})
 
-#Soru7:Şehirlere göre PRICE ortalamaları nedir?
+# Question 7: What are the PRICE averages by city?
 df.groupby("SaleCityName").agg({"Price" : "mean"})
 
-# Soru8:Conceptlere göre PRICE ortalamaları nedir?
+# Question 8: What are the PRICE averages according to concepts?
 df.groupby("ConceptName")["Price"].mean()
 
-# Soru9:Şehir-Concept kırılımında PRICE ortalamaları nedir?
+# Question 9: What are the PRICE averages in the City-Concept breakdown?
 df.groupby(["ConceptName","SaleCityName"])["Price"].mean()
 
 
-# Görev 2: SaleCheckInDay Diffdeğişkenini kategorik bir değişkene çeviriniz.
+# Task 2: Convert the SaleCheckInDay Diff variable to a categorical variable.
 
 bins = [-1, 7, 30, 90, df["SaleCheckInDayDiff"].max()]
 labels = [ "Last Minuters", "Potential Planners", "Planners",  "Early Bookers"]
@@ -40,9 +40,8 @@ labels = [ "Last Minuters", "Potential Planners", "Planners",  "Early Bookers"]
 df["EB_score"] = pd.cut(df["SaleCheckInDayDiff"], bins = bins,labels=labels)
 df.head(50).to_excel("eb_scorew.xlsx", index=False)
 
-#Görev 3:  COUNTRY, SOURCE, SEX, AGE kırılımında ortalama kazançlar nedir?
-#  Şehir-Concept-EB Score, Şehir-Concept-Sezon, Şehir-Concept-CInDay kırılımında ortalama ödenen ücret ve yapılan işlem sayısı cinsinden
-#  inceleyiniz ?
+# Task 3:  What are the average earnings in COUNTRY, SOURCE, SEX, AGE breakdown?
+# Examine the City-Concept-EB Score, City-Concept-Season, City-Concept-CInDay breakdown in terms of average paid wage and number of transactions made
 
 df.groupby(["SaleCityName", "ConceptName", "EB_score"]).agg({"Price" : ["mean","count"]})
 
@@ -54,34 +53,34 @@ df["SaleCityName"]
 
 
 
-#Görev 4:  City-Concept-Season kırılımının çıktısını PRICE'a göre sıralayınız.
+# Task 4: Sort the output of the City-Concept-Season breakdown by PRICE.
 
 agg_df = df.groupby(["SaleCityName", "ConceptName", "Seasons"]).agg({"Price" : "mean"}).sort_values("Price",ascending = False)
 agg_df.head(20)
 
 
-#Görev 5:  Indekste yer alan isimleri değişken ismine çeviriniz.
-#  Üçüncü sorunun çıktısında yer alan PRICE dışındaki tüm değişkenler index isimleridir. Bu isimleri değişken isimlerine çeviriniz.
+# Task 5: Convert the names in the index to variable names.
+# All variables except PRICE in the output of the third question are index names. Convert these names to variable names.
 
 agg_df.reset_index(inplace = True)
 
 agg_df.head()
 
 
-#Görev 6:  Yeni seviye tabanlı müşterileri (persona) tanımlayınız.
-"""Yeni seviye tabanlı satışları tanımlayınız ve veri setine değişken olarak ekleyiniz.
- • Yeni eklenecek değişkenin adı: sales_level_based
- • Önceki soruda elde edeceğiniz çıktıdaki gözlemleri bir araya getirerek sales_level_baseddeğişkenini oluşturmanız gerekmektedir."""
+# Task 6: Define new level-based customers (personas).
+"""Define new level-based sales and add it to the data set as a variable.
+• The name of the new variable to be added: sales_level_based
+• You need to create the sales_level_based variable by bringing together the observations in the output you will obtain in the previous question."""
 
 agg_df['sales_level_based'] = agg_df[["SaleCityName", "ConceptName", "Seasons"]].agg(lambda x: '_'.join(x).upper(), axis= 1)
 
 
 #############################################
-# GÖREV 7: Personaları segmentlere ayırınız.
-#############################################
-# PRICE'a göre 4 segmentlere ayırınız,
-# segmentleri "SEGMENT" isimlendirmesi ile agg_df'e ekleyiniz
-# segmentleri betimleyiniz  • Segmentleri betimleyiniz (Segmentlere göre group by yapıp price mean, max, sum’larını alınız).
+# TASK 7: Segment the personas.
+###########################################
+# Segment into 4 segments according to PRICE,
+# Add the segments to agg_df with the name "SEGMENT"
+# Describe the segments • Describe the segments (Group by segments and get the price mean, max, sum).
 
 agg_df["SEGMENT"] = pd.qcut(agg_df["Price"],4, labels= ["D","C","B","A"])
 agg_df.head(30)
@@ -89,7 +88,7 @@ agg_df.head(30)
 agg_df.groupby("SEGMENT").agg({"Price" : ["mean", "max", "sum"]})
 
 
-#Görev 8:  Yeni gelen müşterileri sınıflandırıp, ne kadar gelir getirebileceklerini  tahmin ediniz.
+# Task 8: Classify new customers and estimate how much revenue they can bring.
 """ Antalya’da herşey dahil ve yüksek sezonda tatil yapmak isteyen bir kişinin ortalama ne kadar gelir kazandırması beklenir?
  • Girne’de yarım pansiyon bir otele düşük sezonda giden bir tatilci hangi segmentte yer alacaktır?"""
 
